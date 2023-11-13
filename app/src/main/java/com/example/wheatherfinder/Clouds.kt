a69c6dd55c3f46b28ce6acc32f8382f8fe109574
ishant <ishantkuwt@gmail.com>
2023-11-13 17:53:04 +0530

    commit 3 backend code started

---
+++ app/src/main/java/com/example/wheatherfinder/Clouds.kt
@@ -0,0 +1,5 @@
+package com.example.wheatherfinder
+
+data class Clouds(
+    val all: Int
+)
---
+++ app/src/main/java/com/example/wheatherfinder/Wind.kt
@@ -0,0 +1,6 @@
+package com.example.wheatherfinder
+
+data class Wind(
+    val deg: Int,
+    val speed: Double
+)
